026165cc3e899119bb878d4006107ba2736776cd
Brandon Garibaldi <bmg.oak@gmail.com>
2017-08-09 14:20:11 -0700

    adds home page and users controllers.  adds routes for home page and login/register

---
+++ app/controllers/landing_pages.rb
@@ -0,0 +1,13 @@
+class LangingPagesController < ApplicationController
+  root to: 'landing_pages#home'
+
+  resources :users
+  resources :sessions
+
+  get '/register' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/register' => 'users#create'
+  post '/login' => 'sessions#create'
+
+  get '/logout' => 'sessions#destroy'
+end
