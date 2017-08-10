557fb68d326081e57a55c6da333f8b1cfcc32488
Tony <saric.tony@gmail.com>
2017-08-10 12:45:40 -0700

    comments out user validations, something with it is breaking th app

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
-  validates: :email, :user_id, uniqueness, true
-  validates: :first_name, :last_name, :password_digest, presence: true
+  #something with the validates is breaking the app.
+
+  # validates: :email, :user_id, uniqueness: true
+  # validates: :first_name, :last_name, :password_digest, presence: true
 end
