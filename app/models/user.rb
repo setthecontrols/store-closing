cbf78d1c9a30df9e1da105a29bffb226611c191c
Tony <saric.tony@gmail.com>
2017-08-10 11:55:52 -0700

    corrects associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many: posts
-  has_many: comments
+  has_many :posts
+  has_many :comments
 
   validates: :email, :user_id, uniqueness, true
   validates: :first_name, :last_name, :password_digest, presence: true
