f260acdc4349c5fe8cb44f26805c4aa0a9f21708
Tony <saric.tony@gmail.com>
2017-08-09 22:55:06 -0700

    adds posts database and comments database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807232439) do
+ActiveRecord::Schema.define(version: 20170810053942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "user_id_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id_id"], name: "index_comments_on_user_id_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "phone_number"
+    t.string "tag"
+    t.date "close_date"
+    t.text "description"
+    t.bigint "user_id_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id_id"], name: "index_posts_on_user_id_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "first_name"
