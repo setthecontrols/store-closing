ba5890c6a53d0f5622deaa0f4a2ff90bb7addf31
Aaron Calander <aaroncalander@yahoo.com>
2017-08-15 09:11:06 -0700

    Adds additional validation to post model for Paperclip images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_attached_file :image, styles: { :medium => "640x"}
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
 
   belongs_to :user
   has_many :comments
